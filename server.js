80fd44050fd379f25e930e428c2351988c180335
raiedev <thaisdivino21@gmail.com>
2024-11-20 18:30:26 -0300

    Conectando db com API e estrutura de pastas.

--- server.js
+++ server.js
@@ -1,41 +1,38 @@
 import express from "express";
-
-const posts = [
-  {
-    descricao: "Foto teste",
-    imagem: "https://placecats.com/millie/300/150",
-    id: 1,
-  },
-  {
-    descricao: "Gato fazendo yoga",
-    imagem: "https://placecats.com/millie/300/150",
-    id: 2,
-  },
-  {
-    descricao: "Gatinho dormindo",
-    imagem: "https://placecats.com/millie/300/150",
-    id: 3,
-  },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
   console.log("Servidor conectando...");
 });
 
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
+// const posts = [
+//   {
+//     descricao: "Foto teste",
+//     imagem: "https://placecats.com/millie/300/150",
+//     id: 1,
+//   },
+//   {
+//     descricao: "Gato fazendo yoga",
+//     imagem: "https://placecats.com/millie/300/150",
+//     id: 2,
+//   },
+//   {
+//     descricao: "Gatinho dormindo",
+//     imagem: "https://placecats.com/millie/300/150",
+//     id: 3,
+//   },
+// ];
 
-function buscarPostPorID(id) {
-  return posts.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
+// function buscarPostPorID(id) {
+//   return posts.findIndex((post) => {
+//     return post.id === Number(id);
+//   });
+// }
 
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPostPorID(req.params.id);
-  res.status(200).json(posts[index]);
-});
+// app.get("/posts/:id", (req, res) => {
+//   const index = buscarPostPorID(req.params.id);
+//   res.status(200).json(posts[index]);
+// });
